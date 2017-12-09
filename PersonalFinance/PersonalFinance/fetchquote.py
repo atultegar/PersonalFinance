e8657780bdb8046e6023270dfca717bfd6aca29d
Atul Tegar <atul.tegar@gmail.com>
2017-12-09 23:13:12 +0530

    commit2
    MF quote from AMFI India

--- PersonalFinance/PersonalFinance/fetchquote.py
+++ PersonalFinance/PersonalFinance/fetchquote.py
@@ -1,3 +1,6 @@
-import clr
-clr.AddReference ('System.Windows.Forms')
-
+from urllib2 import Request, urlopen
+url = 'https://www.amfiindia.com/spages/NAVAll.txt'
+req = Request(url)
+resp = urlopen(req)
+content = resp.read()
+print content
