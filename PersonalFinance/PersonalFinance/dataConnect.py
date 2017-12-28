e66f79bead7af7a271caed879348aec388888e49
Atul Tegar <31208705+atultegar@users.noreply.github.com>
2017-12-28 18:30:56 +0530

    update by upload
    updated

---
+++ PersonalFinance/PersonalFinance/dataConnect.py
@@ -0,0 +1,45 @@
+﻿import sqlite3
+
+class getDataset(object):
+    def __init__(self):
+        conn = sqlite3.connect("mfindia.db")
+        curs = conn.cursor()
+
+        curs.execute ("SELECT schemeType FROM schemeType")    
+        schemeType = curs.fetchall()
+        
+        curs.execute ("SELECT schemeHouse FROM schemeHouse")
+        schemeHouse = curs.fetchall()
+
+        self.schemeTypes = [i[0] for i in schemeType]
+        self.schemeHouses = [j[0] for j in schemeHouse]
+        
+class getSchemes(object):
+    def __init__(self, type1, house1):
+        conn = sqlite3.connect("mfindia.db")
+        curs = conn.cursor()
+        #type1 = 'Open Ended Schemes (ELSS)'
+        #house1 = 'Axis Mutual Fund'
+        query = str.format("SELECT schemeCode, schemeName FROM schemes WHERE schemeType= '{0}' AND schemeHouse= '{1}'", type1, house1)
+        curs.execute(query)
+        self.scheme = curs.fetchall()
+        #self.schemes = [i[1] for i in scheme]
+        #self.schemeId = [i[0] for i in scheme]
+        
+    
+#dataset = getDataset()
+#typeValues = dataset.schemeTypes
+#for typeValue in typeValues:
+#    print typeValue
+
+#houseValues = dataset.schemeHouses
+#for houseValue in houseValues:
+#    print houseValue
+#
+#type1 = 'Open Ended Schemes (ELSS)'
+#house1 = 'Axis Mutual Fund'
+#schemedata = getSchemes(type1, house1)
+##schemeIds = schemedata.schemeId
+#schemeValues = schemedata.scheme
+#for key, data in schemeValues:
+#    print key,":",data
---
+++ PersonalFinance/PersonalFinance/main.py
@@ -0,0 +1,64 @@
+﻿import clr
+import wpf
+import dataConnect
+
+clr.AddReference("PresentationFramework")
+clr.AddReference("PresentationCore")
+
+from System.IO import File
+from System.Windows.Markup import XamlReader
+
+from System.Windows import (
+    Application, Window
+)
+from System.Windows.Controls import *
+
+from System.Collections.ObjectModel import *
+from System.ComponentModel import *
+
+            
+class main(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'main.xaml')
+    
+window = main()
+app = Application()
+combo1 = ComboBox()
+dataset = dataConnect.getDataset()
+datavalues = dataset.schemeTypes
+
+type1 = 'Open Ended Schemes (ELSS)'
+house1 = 'Axis Mutual Fund'
+schemedataset = dataConnect.getSchemes(type1, house1)
+schemevalues = schemedataset.scheme
+
+for datavalue in datavalues:
+    item = ComboBoxItem()
+    item.Content = datavalue
+    combo1.Items.Add(item)
+
+combo2 = ComboBox()
+housevalues = dataset.schemeHouses
+for housevalue in housevalues:
+    item = ComboBoxItem()
+    item.Content = housevalue
+    combo2.Items.Add(item)
+
+combo3 = ComboBox()
+for schemeid, schemevalue in schemevalues:
+    item = ComboBoxItem()
+    item.Content = schemevalue
+    combo3.Items.Add(item)
+
+listbox = ListBox()
+for dataId, datav in schemevalues:
+    litem = ListBoxItem()
+    litem.Content = dataId
+    listbox.Items.Add(litem)
+
+window.ComboBox1.ItemsSource = combo1.Items
+window.ComboBox2.ItemsSource = combo2.Items
+window.ComboBox3.ItemsSource = combo3.Items
+window.ListBox1.ItemsSource = listbox.Items
+
+app.Run(window)
